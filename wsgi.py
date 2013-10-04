7f172dd9dd8c93b10e71f517cda93825ba0cb769
Richard Busby <rsbusby@gmail.com>
2013-10-04 13:08:47 -0700

    Still trying Heroku 10

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ import sys
 root = 'sites/homemade'
 sys.path.insert(0, root)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_heroku")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
