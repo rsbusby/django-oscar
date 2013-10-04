b2df02d9885cda17a110fe62cfe77f756dd5f523
Richard Busby <rsbusby@gmail.com>
2013-10-04 13:23:35 -0700

    same as in root dir , trying to avoid error cannot import apps.homemade module

--- sites/homemade/wsgi.py
+++ sites/homemade/wsgi.py
@@ -1,11 +1,12 @@
 
 import os
+import sys
 
 # Project root
 root = 'sites/homemade'
 sys.path.insert(0, root)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_heroku")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
