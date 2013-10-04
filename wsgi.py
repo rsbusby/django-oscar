6976297dd110430de0e78744cb403ee967d8a3d7
Richard Busby <rsbusby@gmail.com>
2013-10-04 11:45:39 -0700

    Still trying Heroku

---
+++ wsgi.py
@@ -0,0 +1,17 @@
+
+import os
+
+# Project root
+root = 'sites/homemade'
+sys.path.insert(0, root)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+from django.core.wsgi import get_wsgi_application
+
+
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
