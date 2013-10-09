7cf3a830de5bdeea0da2047e899db9ffa3eb209c
Richard Busby <rsbusby@gmail.com>
2013-10-08 23:31:56 -0700

    not using AWS S3 authentication by default

--- sites/homemade/settings_heroku.py
+++ sites/homemade/settings_heroku.py
@@ -68,6 +68,8 @@ STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'foodbucket'
 AWS_PRELOAD_METADATA = True # necessary to fix manage.py collectstatic command to only upload changed files instead of all files
 
+AWS_QUERYSTRING_AUTH = False
+
 MEDIA_ROOT=''
 
 STATIC_URL = 'https://foodbucket.s3.amazonaws.com/'
