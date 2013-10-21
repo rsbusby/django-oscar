1de693f96c4e3cd21ddaaf8593baa5c969d1eb85
Richard Busby <rsbusby@gmail.com>
2013-10-21 12:22:01 -0700

    formatting cart, added give back modal

--- oscar/apps/basket/app.py
+++ oscar/apps/basket/app.py
@@ -17,7 +17,7 @@ class BasketApplication(Application):
     def get_urls(self):
         urlpatterns = patterns('',
             #url(r'^$', self.summary_view.as_view(), name='summary'),
-            #url(r'^$', self.summary_view.as_view(), name='summary'),            
+            url(r'^$', self.summary_view.as_view(), name='single'),            
             url(r'^multi$', self.summary_view.as_view(), name='multi'),
             url(r'^list/$', self.multi_vendor_view.as_view(), name='summary'),
             url(r'^add/$', self.add_view.as_view(), name='add'),
